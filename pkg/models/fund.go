58f134542ecd3efc4d0c75664f206875181a1615
Nghia Nguyen <ngocnghia128@gmail.com>
2023-11-15 07:04:30 +0700

    update

--- pkg/models/fund.go
+++ pkg/models/fund.go
@@ -1,6 +1,23 @@
 package models
 
 type Fund struct {
-	UserID string
+	UserID uint
 	Credit int64
+}
+
+func AddCredit(userID uint, money int64) error {
+	var fund Fund
+	tx := dbGorm.Begin()
+	err := tx.Where("user_id=?", userID).First(&fund).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	fund.Credit += money
+	err = tx.Save(&fund).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit().Error
 }
